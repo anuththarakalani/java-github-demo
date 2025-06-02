18755381adb327c26355edadf06187ed6380968a
anuththarakalani <anuththarakalaniekanayaka@gmail.com>
2025-06-02 13:28:46 +0530

    Added author's name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
  public static void main(String[] args) {
  System.out.println("Hello from GitHub and Java!");
  System.out.println("This code was written by Anuththara");
+ System.out.println("This code was written by you");
  }
 }
